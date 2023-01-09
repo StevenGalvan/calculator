c42372ade272980c2fe0c5c03883b5138478e572
“StevenGalvan” <galvan4739@gmail.com>
2023-01-09 17:39:39 -0600

    ADD refactored code

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
-function saySomething(whatToSay) {
-  window.alert(whatToSay);
-}
-
+// business logic
 function add(number1,number2) {
   return number1 + number2;
 }
 
-saySomething("hi");
+//user interface logic
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
-const result = add(3,5);
-const outputText = "The sum is " + result + ".";
-saySomething(outputText);
+window.alert(add(number1,number2));
+
+saySomething("Welcome to the Calculator");
+
+saySomething("The sum is " + add(3,5) + ".");
