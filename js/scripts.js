db1fd711ba426416e1e7c381b9bddb681aafde0a
“StevenGalvan” <galvan4739@gmail.com>
2023-01-09 17:51:03 -0600

    ADD multiply, divide functions

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,22 @@ function subtract(number1,number2) {
   return number2 - number1;
 }
 
+function multiply(number1,number2) {
+  return number1 * number2;
+}
+
+function divide(number1,number2) {
+  return number1 / number2;
+}
+
 //user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1,number2));
 window.alert(subtract(number2,number1));
+window.alert(multiply(number2,number1));
+window.alert(divide(number2,number1));
 
 saySomething("Welcome to the Calculator");
 
