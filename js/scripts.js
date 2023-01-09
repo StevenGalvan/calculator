17d9df84e928d74e1940bde70704065e949388a4
“StevenGalvan” <galvan4739@gmail.com>
2023-01-09 17:45:24 -0600

    ADD subtraction function to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@
 function add(number1,number2) {
   return number1 + number2;
 }
+function subtract(number1,number2) {
+  return number2 - number1;
+}
 
 //user interface logic
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
 window.alert(add(number1,number2));
+window.alert(subtract(number2,number1));
 
 saySomething("Welcome to the Calculator");
 
